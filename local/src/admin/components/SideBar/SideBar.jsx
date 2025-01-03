2686e4a62266cfc1609674c26cad820374261075
Amigo20057 <denrizuk@gmail.com>
2025-01-03 21:35:31 +0200

    order api

--- local/src/admin/components/SideBar/SideBar.jsx
+++ local/src/admin/components/SideBar/SideBar.jsx
@@ -1,8 +1,18 @@
+import { LogOut } from "lucide-react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./SideBar.module.scss";
 
 export const SideBar = ({ activeBtnOptions, setActiveBtnOptions }) => {
+	const navigate = useNavigate();
+
+	const onClickLogout = () => {
+		if (window.confirm("Ви впевнені, що хочете вийти?")) {
+			window.localStorage.removeItem("token");
+			navigate("/admin");
+		}
+	};
+
 	return (
 		<div className={styles.sideBar}>
 			<h1>ADMIN PANEL</h1>
@@ -32,6 +42,10 @@ export const SideBar = ({ activeBtnOptions, setActiveBtnOptions }) => {
 					</Link>
 				</li>
 			</ul>
+			<div onClick={() => onClickLogout()} className={styles.logout}>
+				<LogOut />
+				<h2>Вийти</h2>
+			</div>
 		</div>
 	);
 };
